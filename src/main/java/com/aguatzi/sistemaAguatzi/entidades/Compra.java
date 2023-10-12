73cb6ac7b1c7fe41a7e5902eedf2f495ba7f077d
miguelfriasd <miguelangelfriasduran@gmail.com>
2023-10-12 14:58:34 -0700

    feat: agregadas entidades CierreCajaLocal Compra Produccion

---
+++ src/main/java/com/aguatzi/sistemaAguatzi/entidades/Compra.java
@@ -0,0 +1,97 @@
+package com.aguatzi.sistemaAguatzi.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import java.util.Objects;
+
+@Entity
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCompra")
+    private int idCompra;
+
+    @Column(name = "tipoInsumo", length = 45, nullable = false)
+    private String tipoInsumo;
+
+    @Column(name = "cantidad", nullable = false)
+    private int cantidad;
+
+    @Column(name = "monto", nullable = false)
+    private int monto;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public Compra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Compra(String tipoInsumo, int cantidad, int monto, Usuario usuario) {
+        this.tipoInsumo = tipoInsumo;
+        this.cantidad = cantidad;
+        this.monto = monto;
+        this.usuario = usuario;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getTipoInsumo() {
+        return tipoInsumo;
+    }
+
+    public void setTipoInsumo(String tipoInsumo) {
+        this.tipoInsumo = tipoInsumo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Compra compra = (Compra) o;
+        return idCompra == compra.idCompra;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCompra);
+    }
+}
+
---
+++ src/main/java/com/aguatzi/sistemaAguatzi/entidades/Produccion.java
@@ -0,0 +1,86 @@
+package com.aguatzi.sistemaAguatzi.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Produccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idProduccion")
+    private int idProduccion;
+
+    @Column(name = "fecha", nullable = false)
+    private Date fecha;
+
+    @Column(name = "cantidad", nullable = false)
+    private int cantidad;
+
+    @ManyToOne
+    @JoinColumn(name = "idCierreCajaLocal", nullable = false)
+    private CierreCajaLocal cierreCajaLocal;
+
+    public Produccion(int idProduccion) {
+        this.idProduccion = idProduccion;
+    }
+
+    public Produccion(Date fecha, int cantidad, CierreCajaLocal cierreCajaLocal) {
+        this.fecha = fecha;
+        this.cantidad = cantidad;
+        this.cierreCajaLocal = cierreCajaLocal;
+    }
+
+    public int getIdProduccion() {
+        return idProduccion;
+    }
+
+    public void setIdProduccion(int idProduccion) {
+        this.idProduccion = idProduccion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public CierreCajaLocal getCierreCajaLocal() {
+        return cierreCajaLocal;
+    }
+
+    public void setCierreCajaLocal(CierreCajaLocal cierreCajaLocal) {
+        this.cierreCajaLocal = cierreCajaLocal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produccion that = (Produccion) o;
+        return idProduccion == that.idProduccion;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProduccion);
+    }
+}
+
