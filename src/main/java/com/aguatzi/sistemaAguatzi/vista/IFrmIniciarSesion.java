6f7c64373ca2c293e19fdbea6cc46260a32fe649
miguelfriasd <miguelangelfriasduran@gmail.com>
2023-10-17 13:43:31 -0700

    feat: agregada interfaz login e instalado bcrypt

---
+++ src/main/java/com/aguatzi/sistemaAguatzi/vista/IFrmIniciarSesion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.aguatzi.sistemaAguatzi.vista;
+
+/**
+ *
+ * @author mig_2
+ */
+public interface IFrmIniciarSesion {
+    
+}
